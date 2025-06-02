452406e7ac9778289985a6190cb9c5b569a16d3b
joindevops07 <naveen@devops.com>
2025-06-02 15:39:58 +0530

    shell varibles

--- 02-varibles.sh
+++ 02-varibles.sh
@@ -1,6 +1,9 @@
 #!/bin/bash/
 
-echo "ramesh: hello suresh how are you "
-echo "suresh: iam good you"
-echo "ramesh: iam also good" 
-echo "suresh: where are you"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: hello $PERSON2 how are you "
+echo "$PERSON2: iam good you"
+echo "$PERSON1: iam also good" 
+echo "$PERSON2: where are you"
