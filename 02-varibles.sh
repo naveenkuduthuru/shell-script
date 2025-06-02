ac89f76abfbb2e3f2575f2a1cc828ff2cdbaf496
joindevops07 <naveen@devops.com>
2025-06-02 15:41:09 +0530

    shell varibles

--- 02-varibles.sh
+++ 02-varibles.sh
@@ -3,7 +3,7 @@
 PERSON1=Ramesh
 PERSON2=Suresh
 
-echo "$PERSON1: hello $PERSON2 how are you "
+echo "$PERSON1: hello $PERSON2 how are you"
 echo "$PERSON2: iam good you"
 echo "$PERSON1: iam also good" 
 echo "$PERSON2: where are you"
